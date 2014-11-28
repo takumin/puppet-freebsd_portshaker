92d3ea86fe5d27bd222c0fb886bc369a0860ef50
takumi <takumi@tmfam.com>
2014-11-29 00:53:09 +0900

    portshaker source configuration

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,4 +3,21 @@
 # This class is called from freebsd_portshaker for service config.
 #
 class freebsd_portshaker::config {
+  file { "$config":
+    ensure  => file,
+    owner   => 'root',
+    group   => 'wheel',
+    mode    => '0644',
+    content => template($config_template),
+  }
+  define portshaker_source ($name = $title, $method) {
+    file { "$source_dir/$name":
+      ensure  => file,
+      owner   => 'root',
+      group   => 'wheel',
+      mode    => '0755',
+      content => template($source_template),
+    }
+  }
+  create_resources(portshaker_source, $source)
 }
