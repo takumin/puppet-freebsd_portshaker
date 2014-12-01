a7c2645204fa0d5c512926f887aa90d8528994fa
takumi <takumi@tmfam.com>
2014-12-01 14:01:16 +0900

    fix

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,21 +3,21 @@
 # This class is called from freebsd_portshaker for service config.
 #
 class freebsd_portshaker::config {
-  file { "$config":
+  file { "$::freebsd_portshaker::config":
     ensure  => file,
     owner   => 'root',
     group   => 'wheel',
     mode    => '0644',
-    content => template($config_template),
+    content => template($::freebsd_portshaker::config_template),
   }
-  define portshaker_source ($param, $source_dir, $source_template) {
-    file { "$source_dir/$name":
+  define portshaker_source ($method) {
+    file { "$::freebsd_portshaker::source_dir/$name":
       ensure  => file,
       owner   => 'root',
       group   => 'wheel',
       mode    => '0755',
-      content => template($source_template),
+      content => template($::freebsd_portshaker::source_template),
     }
   }
-  create_resources(portshaker_source, $source, $source_dir, $source_template)
+  create_resources(portshaker_source, $source)
 }
