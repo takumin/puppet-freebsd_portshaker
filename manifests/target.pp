3bcd1a20e7a96a6a7d6525522d89368811e07c6c
takumin <takumiiinn@gmail.com>
2014-12-27 07:15:00 +0900

    fix

---
+++ manifests/target.pp
@@ -0,0 +1,19 @@
+# == Class freebsd_portshaker::target
+#
+# This class is called from freebsd_portshaker for target ports tree
+#
+define freebsd_portshaker::target (
+  $zfs        = undef,
+  $tree       = undef,
+  $merge      = undef,
+  $preupdate  = undef,
+  $postupdate = undef,
+  $preupdate  = undef,
+  $postupdate = undef,
+) {
+  concat::fragment { "target_$name":
+    target  => $::freebsd_portshaker::config,
+    content => template($::freebsd_portshaker::target_template),
+    order   => '1'
+  }
+}
